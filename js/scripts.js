6b2b0febd7476c7793b8f3baae9c2c76d8ba3af6
Ernest Bruno and Justin Lardani <ernest@>
2018-01-11 16:29:33 -0800

    Add Restart button to reload page. STILL NEEDS FIXING.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,43 @@
-
-
-
 $(document).ready(function() {
 
+  var formGroups = ["#genderGroup", "#politicalGroup", "#ageGroup", "#bobrGroup"];
+  var activeGroup = 0;
   var getNewChar = function() {
-
     var assessString = function(pref) {
-
-      if (typeof(pref) === "undefined") {
+      if (typeof(pref) === "undefined" || pref === ".no-matter") {
         return "";
       } else {
         return pref;
       };
     };
 
+
     var newGenderPref = assessString($("#gender").val());
     var newPoliPref = assessString($("#political").val());
     var newAgePref = assessString($("input:radio[name=age]:checked").val());
     var newTypePref = assessString($("input:radio[name=bobr]:checked").val());
-    return newCharPref = "." + newGenderPref + "." + newPoliPref + "." + newAgePref + "." + newTypePref;
-
-    console.log(newCharPref);
+    return newGenderPref + newPoliPref + newAgePref + newTypePref;
   };
-  
+
   $("#form").submit(function(event) {
 
+    $(".col-md-3").hide();
     var newCharPref = getNewChar();
     $(newCharPref).show();
-    console.log(newCharPref);
+    $(formGroups[activeGroup]).hide();
+    $(formGroups[activeGroup+1]).show();
+    activeGroup += 1;
+
     event.preventDefault();
+  });
+
+  $("#restart").click(function(event) {
+
+    // var activeGroup = 0;
+    // newCharPref = "";
+    // $(".col-md-3").hide();
+    // $(".form-group").hide();
+    // $("#genderGroup").show();
+    location.reload();
   });
 });
